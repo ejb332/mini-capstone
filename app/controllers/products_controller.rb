a9ad0b22010541511b2fce6aeb66a483014252a8
Eric Biel <eric.j.biel@gmail.com>
2016-11-08 20:01:30 -0600

    Add products html page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def products_method
-    @product = products.first
-    render 'products.html.erb'
+  @products = Product.all
+  render 'products.html.erb'
   end
 end

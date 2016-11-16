72cba5cc43a2e8a73ff1790e8f7d74d1ac460b11
Eric Biel <eric.j.biel@gmail.com>
2016-11-15 20:29:17 -0600

    Add styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,25 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    render 'index.html.erb'
+    render "index.html.erb"
   end
 
   def show
     product_id = params[:id]
     @product = Product.find_by(id: product_id)
-    render 'show.html.erb'
+    render "show.html.erb"
   end
 
   def new
-    render 'new.html.erb'
+    render "new.html.erb"
   end
 
   def create
     cheese = Product.new(
-      name: params['name'],
-      price: params['price'],
-      image: params['image'],
-      description: params['description']
+      name: params["name"],
+      price: params["price"],
+      image: params["image"],
+      description: params["description"]
       )
     cheese.save
     flash[:success] = "Product added successfully!"
@@ -29,7 +29,7 @@ class ProductsController < ApplicationController
   def edit
     product_id = params[:id]
     @product = Product.find_by(id: product_id)
-    render 'edit.html.erb'
+    render "edit.html.erb"
   end
 
   def update

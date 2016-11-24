476962f89ab9070b41f13be8e5e0e90b80c64ce4
Eric Biel <eric.j.biel@gmail.com>
2016-11-23 19:21:09 -0600

    Fix hide/show of sign in/out/up links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123024753) do
+ActiveRecord::Schema.define(version: 20161124004416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161123024753) do
     t.datetime "updated_at",                          null: false
     t.string   "in_stock?"
     t.integer  "supplier_id"
+    t.integer  "user_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
